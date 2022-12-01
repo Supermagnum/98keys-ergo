4a3c1afa77f9d5fb1bcc0654c0afbb3201e1bf6e
hakbraley <65477346+hakbraley@users.noreply.github.com>
2022-12-01 16:27:05 -0500

    compiled "i2c_slave.h not found"

--- 98keys-firmware/98keys/98keys.h
+++ 98keys-firmware/98keys/98keys.h
@@ -5,7 +5,7 @@
 
 #define XXX KC_NO
 
-#define LAYOUT( \
+#define LAYOUT_6x9( \
     L00, L01, L02, L03, L04, L05, L06,            /**/            R06, R05, R04, R03, R02, R01, R00, \
     L10, L11, L12, L13, L14, L15, L16, L17,       /**/       R17, R16, R15, R14, R13, R12, R11, R10, \
     L20, L21, L22, L23, L24, L25, L26, L27,       /**/       R27, R26, R25, R24, R23, R22, R21, R20, \
--- 98keys-firmware/98keys/config.h
+++ 98keys-firmware/98keys/config.h
@@ -3,6 +3,15 @@
 
 #pragma once
 
+// /* key matrix size */
+// // Rows are doubled-up
+// #define MATRIX_COLS 9
+// #define MATRIX_ROWS 12
+
+// // wiring of each half    1     2     3     4     5     6     7     8     9
+// #define MATRIX_COL_PINS { GP2,  GP3,  GP10, GP11, GP12, GP13, GP14, GP15, GP20 }
+// #define MATRIX_ROW_PINS { GP4,  GP5,  GP6,  GP7,  GP8,  GP9 }
+
 /*
  * Feature disable options
  *  These options are also useful to firmware size reduction.
@@ -18,11 +27,11 @@
 //#define NO_ACTION_LAYER
 //#define NO_ACTION_TAPPING
 //#define NO_ACTION_ONESHOT
-#define SPLIT_HAND_PIN GPIO26
+#define SPLIT_HAND_PIN GP26
 #define SPLIT_HAND_PIN_LOW_IS_LEFT
 #define USE_I2C
 #define SPLIT_LED_STATE_ENABLE
 #define DEBOUNCE 5
 //SDA pin is GPIO0 , SCL pin is GPIO1
-#define I2C1_SCL_PIN GPIO1
-#define I2C1_SDA_PIN GPIO0
+#define I2C1_SCL_PIN GP1
+#define I2C1_SDA_PIN GP0
